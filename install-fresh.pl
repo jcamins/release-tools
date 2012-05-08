3d780ee808e0ee8c0d67246c0a16917ba42d32a4
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-05-08 11:13:47 -0400

    Improve release test scripts
    Add brief documentation to the scripts, and incorporate webinstaller testing
    for all MARC flavours with both the packages and tarballs.
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- install-fresh.pl
+++ install-fresh.pl
@@ -1,28 +1,52 @@
 #!/usr/bin/perl
 
-use Test::More tests => 5;
-use Test::WWW::Mechanize;
+# Copyright 2012 C & P Bibliography Services
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+#
 
+# Usage: install-fresh.pl intranet-URL opac-URL marcflavour dbuser dbpass
+#
+# This script uses Test::WWW::Mechanize to go through the Koha webinstaller
+# process, and confirm that the end result is a functional Koha installation.
+
+use Test::More tests => 14;
+use Test::WWW::Mechanize;
+use Data::Dumper;
+use HTML::Form;
+
+my ($intranet, $opac, $flavour, $user, $password) = @ARGV;
 my $agent = Test::WWW::Mechanize->new();
 
-$agent->get_ok("$ARGV[0]/cgi-bin/koha/installer/install.pl");
+$agent->get_ok("$intranet/cgi-bin/koha/installer/install.pl", 'open installer');
 $agent->form_name('mainform');
-$agent->field('password', 'koharel');
-$agent->field('userid', 'koharel');
-$agent->click();
+$agent->field('password', $password);
+$agent->field('userid', $user);
+$agent->click_ok('', 'log in to installer');
 
 $agent->form_name('language');
 $agent->field('language', 'en');
-$agent->click();
+$agent->click_ok('', 'choose language');
 
 $agent->form_name('checkmodules');
-$agent->click();
+$agent->click_ok('', 'check modules');
 
 $agent->form_name('checkinformation');
-$agent->click();
+$agent->click_ok('', 'check information');
 
 $agent->form_name('checkdbparameters');
-$agent->click();
+$agent->click_ok('', 'check db parameters');
 
 $agent->form_number(1);
 $agent->click();
@@ -32,53 +56,29 @@ $agent->click();
 
 $agent->follow_link(text => 'install basic configuration settings', n => '1');
 $agent->form_name('frameworkselection');
-$agent->field('marcflavour', 'MARC21');
-$agent->click();
+$agent->field('marcflavour', $flavour);
+$agent->click_ok('', 'framework selection');
 
 $agent->form_name('frameworkselection');
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/sample_creator_data.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/userflags.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/sample_notices_message_transports.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/sample_libraries.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/sample_news.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/patron_atributes.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/patron_categories.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/sample_notices.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/marcflavour/marc21/mandatory/marc21_framework_DEFAULT.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/sample_itemtypes.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/sample_holidays.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/marc21_holdings_coded_values.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/sample_notices_message_attributes.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/auth_val.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/marcflavour/marc21/mandatory/authorities_normal_marc21.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/message_transport_types.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/auth_values.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/marcflavour/marc21/optional/marc21_simple_bib_frameworks.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/sample_patrons.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/userpermissions.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/parameters.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/stopwords.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/marcflavour/marc21/optional/marc21_fastadd_framework.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/marcflavour/marc21/optional/marc21_default_matching_rules.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/subtag_registry.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/optional/sample_z3950_servers.sql");
-$agent->tick('framework', "$ARGV[2]/koha/intranet/cgi-bin/installer/data/mysql/en/mandatory/class_sources.sql");
-$agent->click();
+for (my $_ = $agent->content(); pos($_) < length($_) && $_ =~ m/<input type="checkbox" name="framework" value="([^"]*)"/g; ) {
+    $agent->tick('framework', $1);
+}
+$agent->click_ok('', 'additional data');
 
 $agent->form_name('finish');
-$agent->click();
+$agent->click_ok('', 'finish');
 
 $agent->follow_link(text => 'here', n => '1');
 $agent->form_name('loginform');
-$agent->field('password', 'koharel');
-$agent->field('userid', 'koharel');
+$agent->field('password', $password);
+$agent->field('userid', $user);
 $agent->field('branch', '');
-$agent->click();
+$agent->click_ok('', 'login to staff client');
 
-$agent->get_ok("$ARGV[0]");
+$agent->get_ok("$intranet");
 $agent->content_contains("Welcome to Koha", "intranet news");
 
 
-$agent->get_ok("$ARGV[1]");
+$agent->get_ok("$opac");
 $agent->content_contains("Welcome to Koha...", "opac main user block");
 
